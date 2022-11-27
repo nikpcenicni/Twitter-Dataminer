0b2c826cf6341c943bed7cb099918f524f9403c6
Nikholas Pcenicni <82239765+nikpcenicni@users.noreply.github.com>
2022-11-26 22:37:26 -0500

    cleaned up main.py and fixed mentions and replys methods

--- main.py
+++ main.py
@@ -1,28 +1,48 @@
 from analyzer import *
 from twitter import *
 
+import os
 
-def replys():
-    pass
 
-def mentions():
+def reset():
+    open("tweet_list.txt", "w").close()
+
+
+def replys(screen_name):
+    
+    tweets = get_user_tweets(screen_name)
+    i = 0
+    for tweet in tweets:
+        print(f"{i}: {tweet.text}")
+        i+=1
+        
+    id = int(input("Enter the number of the tweet you want to analyze: "))
+    tweet_id = tweets[id].id
+    
+    tweets = get_replies(tweet_id, screen_name)
+    
+    return tweets
+    # = get_replies(tweet_id, screen_name)
+    
+
+def mentions(screen_name):
+    tweets = get_mentions(screen_name)
+    return tweets
     pass
 
 
 def operations(screen_name):
     operation = input("Enter 1 to analyze tweet replies, 2 to analyze mentions, or any other value to quit: ")
     if operation == '1':
-        tweet_id = input("Enter tweet id: ")
-        tweets = get_replies(tweet_id, screen_name)
+        tweets =  replys(screen_name)
         return tweets
     elif operation == '2':
-        tweets = get_mentions(screen_name)
+        tweets = mentions(screen_name)
         return tweets
     else:
         return None
 
-
-def main():
+def user_input():
     screen_name = input("Enter a Twitter handle: ")
     # if screen_name[0] == '@':
     #     screen_name = screen_name[1:]
@@ -34,6 +54,11 @@ def main():
     print("Number of Friends: " + str(user.friends_count))
     print("Number of Tweets: " + str(user.statuses_count))
     print("Number of Likes: " + str(user.favourites_count))
+    return screen_name
+
+def main():
+    
+    screen_name = user_input()
     
     tweets = operations(screen_name)
     if tweets != None:
@@ -55,5 +80,6 @@ def main():
 
     # # Print the sentiment
     # print(sentiment)
+    reset()
     
 main()
