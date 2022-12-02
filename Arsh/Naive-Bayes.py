24ca313125982cb4fefb9c19e283007083768598
arshjit pelia <arsh.pelia1@gmail.com>
2022-12-02 18:48:17 -0500

    added graphs and wordcloud

--- Arsh/Naive-Bayes.py
+++ Arsh/Naive-Bayes.py
@@ -15,6 +15,7 @@
 
 #import packages
 #general purpose packages
+import itertools
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -157,6 +158,7 @@ def clean_data():
 
     #call naive_bayes function
     naive_bayes(X_train, X_test, y_train_le, y_test_le)
+
     return df
 
 def naive_bayes(X_train, X_test, y_train_le, y_test_le):
@@ -178,6 +180,17 @@ def naive_bayes(X_train, X_test, y_train_le, y_test_le):
     #classification report
     print(classification_report(y_test_le, nb_predictions, target_names= ['Negative','Neutral','Positive']))
     print()
+
+    #confusion matrix
+    cm = confusion_matrix(y_test_le, nb_predictions)
+    # print(cm)
+    #plot confusion matrix
+    plt.figure(figsize=(10,7))
+    sns.heatmap(cm, annot=True)
+    plt.xlabel('Predicted')
+    plt.ylabel('Truth')
+    plt.show()
+
 
 def get_sentiment(tweet):
     #get sentiment using vaderSentiment and classify tweets as extreme positive, positive, neutral, negative, extreme negative
@@ -223,11 +236,67 @@ def clean_tweet(tweet):
     tweet = tweet.lower()
     return tweet
 
+#create wordcloud
+def wordcloud(df):
+    #create wordcloud
+    all_words = ' '.join([text for text in df['tweet_OG']])
+    wordcloud = WordCloud(width=800, height=500, random_state=21, max_font_size=110).generate(all_words)
+    plt.figure(figsize=(10, 7))
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis('off')
+    plt.show()
+
+#plot confusion matrix
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+#create bar chart
+def bar_chart(df):
+    #create bar chart
+    df['Sentiment'].value_counts().plot(kind='bar')
+    plt.title('Sentiment Analysis')
+    plt.xlabel('Sentiment')
+    plt.ylabel('Count')
+    plt.show()
+
+
 def main():
     # create_CSV()
     df = clean_data()
+    wordcloud(df)
+    bar_chart(df)
     # df.info()
     # print(df.head())
+
 
 if __name__ == "__main__":
     main()
