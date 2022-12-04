3c9de3c15354e81fc26e35bab0b85f5b79285e5c
Nikholas Pcenicni <82239765+nikpcenicni@users.noreply.github.com>
2022-12-03 19:47:42 -0500

    parameterized hatespeech classifiy, need to integreate with naive-bayer

--- hatespeech.py
+++ hatespeech.py
@@ -3,24 +3,38 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-
 import re
 import nltk
 from nltk.util import pr
-stemmer = nltk.SnowballStemmer('english')
 from nltk.corpus import stopwords
 import string
-stopword = set(stopwords.words('english'))
 
-hate = pd.read_csv('hatespeech.csv')
-hate['labels'] = hate['class'].map({0:'Hate Speech Detected', 1:'Offensive Language Detected', 2:'No Hate Speech Detected'})
-
-hate = hate[['tweet', 'labels']]
-hate.head()
+def train_classifier():
+    stemmer = nltk.SnowballStemmer('english')
 
 
+    hate = pd.read_csv('hatespeech.csv')
+    hate['labels'] = hate['class'].map({0:'Hate Speech Detected', 1:'Offensive Language Detected', 2:'No Hate Speech Detected'})
+
+    hate = hate[['tweet', 'labels']]
+    hate.head()
+
+    hate['tweet'] = hate['tweet'].apply(lambda x: clean_text(x))
+    # print(hate.head())
+        
+    x = np.array(hate['tweet'])
+    y = np.array(hate['labels'])
+
+    cv = CountVectorizer()
+    x = cv.fit_transform(x)
+    x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.33, random_state=42)
+
+    clf = DecisionTreeClassifier()
+    clf.fit(x_train, y_train)
+    return cv, clf
 
 def clean_text(text):
+    stopword = set(stopwords.words('english'))
     text = str(text).lower()
     text = re.sub(r'\[.*?\]', '', text)
     text = re.sub(r'https?://\S+|www\.\S+', '', text)
@@ -32,23 +46,19 @@ def clean_text(text):
     text = " ".join(text)
     return text
 
-hate['tweet'] = hate['tweet'].apply(lambda x: clean_text(x))
-# print(hate.head())
-print(hate.head())
-    
-x = np.array(hate['tweet'])
-y = np.array(hate['labels'])
 
-cv = CountVectorizer()
-x = cv.fit_transform(x)
-x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.33, random_state=42)
+def classify(cv, clf, text):
+    text = clean_text(text)
+    text = cv.transform([text]).toarray()
+    return clf.predict(text)
 
-clf = DecisionTreeClassifier()
-clf.fit(x_train, y_train)
 # print (clf.score(x_test, y_test))
 
-test = 'Fuck you'
+def test():
+    cv, clf = train_classifier()
+    classification = classify(cv, clf, "I hate you")
+    print(classification)
+    classification = classify(cv, clf, "Kill yourself")
+    print(classification)
 
-hate = cv.transform([test]).toarray()
-print(clf.predict(hate))
- 
+test()
