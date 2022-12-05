2c9c083eeec677f813286a5b6b173ff5a036acc3
Nikholas Pcenicni <82239765+nikpcenicni@users.noreply.github.com>
2022-12-04 23:53:55 -0500

    cleaned up network and removed resume

--- network.py
+++ network.py
@@ -55,7 +55,6 @@ def build_network(username, type, length):
     else:
         master_followers = followers
         pass
-    #master_followers = pd.DataFrame({'to':user.id,'from':api.get_follower_ids(user_id=user.id),"username":user.screen_name})
     master_user_details = pd.DataFrame([user_details])
 
     # Setting up skip_listlist
@@ -100,7 +99,6 @@ def build_network(username, type, length):
                     
                     # Getting followers and appending to master list
                     followers = pd.DataFrame({'to':user.id,'from':api.get_follower_ids(user_id=user.id),"username":user.screen_name})
-                    # followers = pd.DataFrame({'from':user.id,'to':api.get_follower_ids(user_id=user.id),"username":user.screen_name})
                     if followers.shape[0] > length:
                         followers = followers.sample(length)
                     else:
@@ -135,85 +133,6 @@ def build_network(username, type, length):
                 print ('Error: ',e)
                 continue
     return master_followers, master_user_details
-
-def resume_network(username, type):
-    out_user, out_followers, out_skip = set_globals(type)
-    # Setting up data frames with initial
-    master_user_details = pd.read_csv(out_user)
-    master_followers = pd.read_csv(out_followers)
-    skip_df = pd.read_csv(out_skip)
-
-    skip_list = []
-    skip_list = list(skip_df['id'])
-
-    last_id = master_user_details['id'].tail(1)
-    last_id = last_id.iloc[0]
-    last_id_idx = master_followers[master_followers['to'] == last_id]
-    last_id_idx = last_id_idx.head(1)
-    last_id_idx = last_id_idx.index.values[0]
-
-    queue_list = list(master_followers['to'].iloc[(last_id_idx+1):,])
-
-    list(map(q.put,queue_list))
-    print(len(list(q.queue)))
-
-    while not q.empty():
-        u = q.get()
-        if u in skip_list:
-            continue
-        else:
-                try:
-                    # API call to get user data
-                    user = api.get_user(user_id=u)
-                    user_details = {'name':user.name,
-                                    'screen_name':user.screen_name,
-                                    'created_at':str(user.created_at),
-                                    'id':user.id,
-                                    'friends_count':user.friends_count,
-                                    'followers_count':user.followers_count}
-                    
-                    # Adding to skip list
-                    skip_list.append(user_details['id'])
-                    skip_df = pd.DataFrame({'id':user.id},index=[0])
-                    
-                    # Appending user data to master list
-                    user_details = pd.DataFrame([user_details])
-                    master_user_details = master_user_details.append(user_details)
-                    
-                    # Getting followers and appending to master list
-                    followers = pd.DataFrame({'from':user.id,'to':api.get_follower_ids(user_id=user.id)})
-                    if followers.shape[0] > 200:
-                        followers = followers.sample(200)
-                    else:
-                        pass
-                    master_followers = master_followers.append(followers)
-                    
-                    # Adding retrieved followers to queue
-                    list(map(q.put,followers['to']))
-                                
-                    # Exporting user and followers to CSV
-                    user_details.to_csv(out_user,index=False,mode='a',header=False)
-                    followers.to_csv(out_followers,index=False,mode='a',header=False)
-                    skip_df.to_csv(out_skip,index=False,mode='a',header=False)
-                    
-                    print (len(list(q.queue)))
-                    
-                # Error handling
-                except tweepy.TweepError as error:
-                    print (type(error))
-            
-                    if str(error) == 'Not authorized.':
-                        print ('Can''t access user data - not authorized.')
-                        skip_list.append(u)
-                        skip_df = pd.DataFrame({'id':u},index=[0])
-                        skip_df.to_csv(out_skip,index=False,mode='a',header=False)
-            
-                    if str(error) == 'User has been suspended.':
-                        print ('User suspended.')
-                        skip_list.append(u)
-                        skip_df = pd.DataFrame({'id':u},index=[0])
-                        skip_df.to_csv(out_skip,index=False,mode='a',header=False)    
-
 
 def view_network(type):
     edge = ''
